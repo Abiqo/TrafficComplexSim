45ae450843831cdbbc1922338aeae52d58ab4ce7
zeqiri <33279307+Abiqo@users.noreply.github.com>
2018-12-13 08:28:00 +0100

    Minor fixes in main and CreateParameters
    For deviation plots

--- CreateParameters.m
+++ CreateParameters.m
@@ -1,9 +1,9 @@
 function p = CreateParameters(p)
-
+p.runs = 4;
 p.t = 0;
-p.nSteps = 5;
+p.nSteps = 7;
 p.waitingTime=20; % period of traffic light waiting time
-p.nAgents_start = 200;
+p.nAgents_start = 400;
 p.nAgents = p.nAgents_start;
 p.storedCars = [];
 p.savedState = [];
@@ -11,7 +11,7 @@ p.storedPositions = [];
 p.time = [];
 p.nTimesteps = 1000;
 p.previousStep = [];
-p.N = 17; % N has to be a number of size k*nSteps+2 in order for the city to have a road in the outer bounds
+p.N = 30; % N has to be a number of size k*nSteps+2 in order for the city to have a road in the outer bounds
 %Create propability vector that it is a car/bike
 p.probOfState = CreateStateProportions;
 p.cityMap = InitGrid(p);
@@ -21,5 +21,5 @@ p.agentsAtIntersection = [];
 p.carTimes = [];
 p.bikeTimes = [];
 p.currentProportionIndex = 0;
-p.proportionHistory.bikes = zeros(size(p.probOfState,1),1);
-p.proportionHistory.cars = zeros(size(p.probOfState,1),1);
+p.bikeHistory = [];
+p.carHistory = [];
