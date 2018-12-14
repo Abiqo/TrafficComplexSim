5818f32e13c325ea51172849391207a8ecfc562a
VictorGardi <45388014+VictorGardi@users.noreply.github.com>
2018-12-14 13:53:32 +0100

    Add files via upload

--- PlotGraphs.m
+++ PlotGraphs.m
@@ -21,7 +21,7 @@ errBike = errBike/sqrt(p.runs);
 
 
 
-e1 = errorbar(flipud(carProportions),meanBike,errBike);
+e1 = errorbar(flipud(carProportions),meanBike,errBike,'LineWidth',4);
 e1.Color = 'red';
 e1.DisplayName = 'Bikes';
 e1.Marker = 'o';
@@ -39,16 +39,17 @@ end
 errCar = errCar/sqrt(p.runs);
 
 
-e2 = errorbar(carProportions,meanCar,errCar);
+e2 = errorbar(carProportions,meanCar,errCar,'LineWidth',4);
 e2.Color = 'blue';
 e2.DisplayName = 'Cars';
 e2.Marker = 'square';
 e2.MarkerSize = 5; 
 e2.MarkerEdgeColor = 'blue';
 e2.MarkerFaceColor = 'blue';
-xlabel('Car proportions','FontSize',20); ylabel('Mean travel time','FontSize',20); legend;
+xlabel('Car proportions','FontSize',40); ylabel('Mean travel time','FontSize',40); 
+legend('FontSize',40);
 text1 = ['Number of runs =  %d'];
 text2 = ['Number of agents = %d'];
 str1 = sprintf(text1,p.runs);
 str2 = sprintf(text2,p.nAgents_start);
-title({str1,str2})
+title({str1,str2},'FontSize',40)
